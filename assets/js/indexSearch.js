cc6b9ccb0f5e44a0ab0a15b6f1f260733ecbbcdf
LuckyLukic <lucabjjiannice@gmail.com>
2023-05-19 13:41:27 +0200

    search

--- assets/js/indexSearch.js
+++ assets/js/indexSearch.js
@@ -1,6 +1,16 @@
-const voloreInput = document.querySelector("input");
-const search = document.querySelector("button")
+const valoreInput = document.querySelector("input");
+const search = document.querySelector("a")
 
 search.addEventListener("click", () => {
-
+    const valore = valoreInput.value
+    search.setAttribute("href", `indexSearchResult.html?q=${valore}`)
 })
+
+
+valoreInput.value.addEventListener("keypress", (evt){
+    if (evt.key === "Enter" && !valoreInput.value == "") {
+        evt.preventDefault();
+        document.getElementById("myBtn").click();
+    }
+});
+
