da507a40345bc3d37e47661212bed9220034512c
LuckyLukic <lucabjjiannice@gmail.com>
2023-05-19 14:23:53 +0200

    search result

--- assets/js/indexSearch.js
+++ assets/js/indexSearch.js
@@ -1,16 +1,20 @@
 const valoreInput = document.querySelector("input");
 const search = document.querySelector("a")
 
+// console.log(search)
+
 search.addEventListener("click", () => {
     const valore = valoreInput.value
-    search.setAttribute("href", `indexSearchResult.html?q=${valore}`)
+    search.setAttribute("href", `indexSearchResult.html?q=${valore.toLowerCase()}`)
 })
 
 
-valoreInput.value.addEventListener("keypress", (evt){
-    if (evt.key === "Enter" && !valoreInput.value == "") {
-        evt.preventDefault();
-        document.getElementById("myBtn").click();
+valoreInput.addEventListener("keypress", (evt) => {
+    const valore = valoreInput.value
+    if (evt.key === "Enter" && !valore == "") {
+        search.setAttribute("href", `indexSearchResult.html?q=${valore.toLowerCase()}`)
+        search.click();
     }
 });
+
 
